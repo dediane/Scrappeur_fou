190c0aeacf6a8454b8cac532e13352c94df8e975
Diane Decourt <dianedecourt@gmail.com>
2020-10-08 14:15:22 +0200

    first commit

---
+++ lib/00_Dark_trader.rb
@@ -0,0 +1,25 @@
+require 'nokogiri'
+require 'open-uri'
+
+page = Nokogiri::HTML(URI.open("https://coinmarketcap.com/all/views/all/"))
+
+all_currency_symbols = page.xpath('/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[3]/div').map{|x| x.text}
+all_currency_prices = page.xpath('/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div/div[2]/div[3]/div/table/tbody/tr/td[5]/a').map{|x| x.text.delete('$').to_f}
+
+#puts all_currency_symbols
+#puts all_currency_prices
+
+def get_array_of_hash(all_currency_symbols, all_currency_prices)
+  array = []
+  if all_currency_prices.length == all_currency_symbols.length
+    
+    all_currency_symbols.length.times do |i|
+      result_hash = {}
+      result_hash[all_currency_symbols[i]] = all_currency_prices[i]
+      array.push(result_hash)
+    end
+  end
+  return array
+end
+
+puts get_array_of_hash(all_currency_symbols, all_currency_prices)
