49f8c970eeaac900c6aac394257e79a8c2fe63dc
Diane Decourt <dianedecourt@gmail.com>
2020-10-08 23:31:58 +0200

    updates on last exercise

--- lib/02_Cher_depute.rb
+++ lib/02_Cher_depute.rb
@@ -4,6 +4,9 @@ require 'open-uri'
 def get_one_mail(url_depute)
   page = Nokogiri::HTML(URI.open(url_depute))
   mail = page.xpath'/html/body/div/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[4]/ul/li[2]/a/text()'
+    if mail == nil
+      page.xpath'/html/body/div/div[3]/div/div/div/section[1]/div/article/div[3]/div/dl/dd[3]/ul/li[2]/a/text()'
+    end
   return mail.to_s
 end 
 
@@ -18,8 +21,11 @@ end
 
 def get_array_first_names
   page = get_page
-  array_of_names = page.xpath('/html/body/div/div[3]/div/div/section/div/article/div[3]/div/div[3]/div/ul/li').map{|x| x.text.split.delete_at(1)}
+  array_of_names = page.xpath('/html/body/div/div[3]/div/div/section/div/article/div[3]/div/div[3]/div/ul/li').map{|x| x.text.split}
+  array_of_names = array_of_names.collect{|x|x[1]}
 end
+
+
 
 def get_array_last_names
   page = get_page
@@ -62,11 +68,11 @@ end
 # puts get_array_first_names.length
 # puts get_array_mails
 # puts get_array_mails.length
-#puts get_array_last_names
+# puts get_array_last_names
 #puts "a = ["
 
 #-- Puts the array with no "styling"
-#puts make_final_array
+# puts make_final_array
 
 #-- Puts the array as in the exemple
 make_final_array.each do |name| 
