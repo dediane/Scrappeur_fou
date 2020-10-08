f8e09365bcf2389f8628a021cd8b540e3f8d72b0
Diane Decourt <dianedecourt@gmail.com>
2020-10-08 20:51:07 +0200

    update 02

--- lib/02_Cher_depute.rb
+++ lib/02_Cher_depute.rb
@@ -7,18 +7,58 @@ def get_one_mail(url_depute)
   return mail.to_s
 end 
 
-def get_array_urls
+def get_page
   page = Nokogiri::HTML(URI.open('http://www2.assemblee-nationale.fr/deputes/liste/alphabetique'))
-  array_of_urls = page.xpath('/html/body/div/div[3]/div/div/section/div/article/div[3]/div/div[3]/div/ul/li/a/@href').map{|x| "http://www.assemblee-nationale.fr" + x.text }
 end
 
-def get_array_names
-  page = Nokogiri::HTML(URI.open('http://www2.assemblee-nationale.fr/deputes/liste/alphabetique'))
-  array_of_names = page.xpath('/html/body/div/div[3]/div/div/section/div/article/div[3]/div/div[3]/div/ul/li').map{|x| x.text.split}
+def get_array_urls
+  page = get_page
+  array_of_urls = page.xpath('/html/body/div/div[3]/div/div/section/div/article/div[3]/div/div[3]/div/ul/li/a/@href').map{|x| "http://www2.assemblee-nationale.fr" + x.text }
 end
+
+def get_array_first_names
+  page = get_page
+  array_of_names = page.xpath('/html/body/div/div[3]/div/div/section/div/article/div[3]/div/div[3]/div/ul/li').map{|x| x.text.split.delete_at(1)}
+end
+
+def get_array_last_names
+  page = get_page
+  array_of_names = page.xpath('/html/body/div/div[3]/div/div/section/div/article/div[3]/div/div[3]/div/ul/li').map{|x| x.text.split}
+  array_of_names = array_of_names.collect{|x|x[2..-1].join(" ")}
+end
+
+def get_array_mails
+  array_of_urls = get_array_urls
+  array_of_mails = array_of_urls.map{|url| get_one_mail(url)}
+end
+
+
+def make_final_array
+  first_names = get_array_first_names
+  last_names = get_array_last_names
+  emails = get_array_mails
+  array = []
+  
+  if first_names.length == last_names.length
+    first_names.length.times do |i|
+      final_hash = {}
+      final_hash["First name:"] = first_names[i]
+      final_hash["Last name:"] = last_names[i]
+      final_hash["Emails:"] = emails[i]
+      array << final_hash
+    end
+    return array
+  end
+end
+
+
+
+
 
 
 #puts get_array_urls
 #puts get_array_urls.length
-puts get_array_names
+#puts get_array_first_names
+#puts get_array_mails
 #puts get_array_last_names
+puts make_final_array
